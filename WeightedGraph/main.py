cd2f78773a9367e56cc65fff64a111c7f637d5ad
yk9978 <ammar.mahmood@kit.edu>
2022-09-16 17:24:21 +0200

    introduced weighted graph

--- WeightedGraph/main.py
+++ WeightedGraph/main.py
@@ -7,12 +7,9 @@ class Graph:
             self.adjMatrix.append([0 for i in range(size)])
         self.size = size
     
-    def add_edge(self,v1,v2):
-        if v1==v2:
-            print("Same vertex %d and %d" % (v1,v2))
-        self.adjMatrix[v1][v2] = 1
-        self.adjMatrix[v2][v1] = 1
-    
+    def add_edge(self,v1,v2,weight='e'):
+        self.adjMatrix[v1][v2] = weight
+
     def remove_edge(self,v1,v2):
         if self.adjMatrix[v1][v2] == 0:
             print("No edge between %d and %d" % (v1,v2))
@@ -24,7 +21,7 @@ class Graph:
         v.append(s)
 
         for i in range(len(self.adjMatrix)):
-            if self.adjMatrix[s][i] == 1 and i not in v:
+            if self.adjMatrix[s][i] != 0 and i not in v:
                 self.dfs(i, v)
         return v
 
@@ -34,16 +31,27 @@ class Graph:
 
 if __name__ == "__main__":
     
-    g = Graph(6)
+    g = Graph(7)
 
+    g.add_edge(0,0,0.35)
     g.add_edge(0,1)
-    g.add_edge(0,2)
-    g.add_edge(3,1)
-    g.add_edge(3,2)
+    g.add_edge(0,3,0.39)
+    g.add_edge(1,3,0.39)
+    g.add_edge(1,0)
+    g.add_edge(2,4,0.70)
+    g.add_edge(4,2,0.90)
+    g.add_edge(3,3,0.3)
+    g.add_edge(3,0,0.91)
+    g.add_edge(3,1,0.91)
     g.add_edge(3,4)
-    g.add_edge(3,4)
-    g.add_edge(4,5)
+    g.add_edge(4,3)
+    g.add_edge(4,4,0.3)
+    g.add_edge(3,5,0.3)
+    g.add_edge(5,3,0.9)
+    g.add_edge(4,5,0.3)
+    g.add_edge(5,4,0.9)
+    g.add_edge(5,6)
+    g.add_edge(6,5)
+    g.add_edge(6,6)
 
     g.print_matrix()
-    v = g.dfs(3)
-    print(v)
